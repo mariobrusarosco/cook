d95ed59eca0228b27f28e619397c5df425e6156c
Mario Brusarosco <mariobrusarosc@hotmail.com>
2020-10-17 13:47:28 -0300

    Finish Lecture 151

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,24 @@ import User from "./models/User";
 console.log("Cook js");
 
 const user = new User({
+  id: 1,
   name: "Mario",
   age: 33,
 });
 
+
 user.set({ age: 10 });
+user.save()
+user.fetch()
 
-const userAge = user.get("age");
+// const userAge = user.get("age");
 
-console.log({ userAge });
+// console.log({ userAge });
 
-user.on("click", () => console.log(1));
-user.on("click", () => console.log(2));
-user.on("hover", () => console.log(3));
+// user.on("click", () => console.log(1));
+// user.on("click", () => console.log(2));
+// user.on("hover", () => console.log(3));
 
-user.trigger("click");
-user.trigger("hover");
+// user.trigger("click");
+// user.trigger("hover");
+
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface UserProps {
+  id: string;
   age: number;
   name: string;
 }
@@ -16,23 +19,41 @@ class User {
     return this.data[propName];
   }
 
-  set(update: Partial<UserProps>): void {
+  set(update: Partial<UserProps>) {
     Object.assign(this.data, update);
   }
 
-  on(eventName: string, callback: Callback): void {
+  on(eventName: string, callback: Callback) {
     this.events[eventName]
       ? this.events[eventName] = [...this.events[eventName], callback]
       : this.events[eventName] = [callback];
   }
 
-  trigger(eventName: string): void {
+  trigger(eventName: string) {
     const eventCallbacks = this.events[eventName];
     const emptyEvent = !eventCallbacks || !eventCallbacks?.length;
 
     if (emptyEvent) return;
 
     eventCallbacks.forEach((callback: Callback) => callback());
+  }
+
+  fetch() {
+    axios.get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse) => {
+        console.log({ response })
+        this.set(response.data)
+    }))
+  }
+
+  save() {
+    const id = this.get('id') 
+
+    if(id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data)
+    }else {
+      axios.post('http://localhost:3000/users', this.data)
+    }
   }
 }
 
